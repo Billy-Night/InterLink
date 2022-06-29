0f5d532cdd2240105048a0b9bc72c87618bdc925
fejul <juliafeller420@gmail.com>
2022-06-29 17:28:30 +0200

    added fake data

--- src/projectData.js
+++ src/projectData.js
@@ -1,96 +1,90 @@
-import React from "react";
+import React from 'react';
+import Hoteliq from './Hoteliq';
 
-
-export const projectData = [{
-    id: 1,
-    poject_name: "AutoScript",
-    progect_image: "",
-    project_location: "",
-    project_type: "",
-    project_description: "",
-    project_industry: "",
-    project_technologies: [
-        "React", "JavaScript", "Express", "Node.js", "MySQL" 
-    ],
-    project_team: {
-        ProjectManager: "",
-        ScrumMaster: "",
-        TechLead: "",
-        WebDeveloper: "",
-        JuniorDeveloper: "",
-        DevOps: "",
-    },
-    project_resources: "",
-    employee_views: "",
-    request_access: ""
-},
-{
-    id: 2,
-    poject_name: "",
-    progect_image: "",
-    project_location: "",
-    project_type: "",
-    project_description: "",
-    project_industry: "",
-    project_technologies:  [
-        "React", "JavaScript", "Express", "Node.js", "MySQL" 
-    ],
-    project_team: {
-        ProjectManager: "",
-        ScrumMaster: "",
-        TechLead: "",
-        WebDeveloper: "",
-        JuniorDeveloper: "",
-        DevOps: "",
-    },
-    project_resources: "",
-    employee_views: "",
-    request_access: ""
-},
-{
-    id: 3,
-    poject_name: "",
-    progect_image: "",
-    project_location: "",
-    project_type: "",
-    project_description: "",
-    project_industry: "",
-    project_technologies:  [
-        "React", "JavaScript", "Express", "Node.js", "MySQL" 
-    ],
-    project_team: {
-        ProjectManager: "",
-        ScrumMaster: "",
-        TechLead: "",
-        WebDeveloper: "",
-        JuniorDeveloper: "",
-        DevOps: "",
-    },
-    project_resources: "",
-    employee_views: "",
-    request_access: ""
-},
-{
-    id: 4,
-    poject_name: "",
-    progect_image: "",
-    project_location: "",
-    project_type: "",
-    project_description: "",
-    project_industry: "",
-    project_technologies:  [
-        "React", "JavaScript", "Express", "Node.js", "MySQL" 
-    ],
-    project_team: {
-        ProjectManager: "",
-        ScrumMaster: "",
-        TechLead: "",
-        WebDeveloper: "",
-        JuniorDeveloper: "",
-        DevOps: "",
-    },
-    project_resources: "",
-    employee_views: "",
-    request_access: ""
-},
+export const projectData = [
+	{
+		id: 1,
+		poject_name: 'AutoScript',
+		progect_image: '',
+		project_location: '',
+		project_type: '',
+		project_description: '',
+		project_industry: '',
+		project_technologies: [ 'React', 'JavaScript', 'Express', 'Node.js', 'MySQL' ],
+		project_team: {
+			ProjectManager: '',
+			ScrumMaster: '',
+			TechLead: '',
+			WebDeveloper: '',
+			JuniorDeveloper: '',
+			DevOps: ''
+		},
+		project_resources: '',
+		employee_views: '',
+		request_access: ''
+	},
+	{
+		id: 2,
+		poject_name: '',
+		progect_image: '',
+		project_location: '',
+		project_type: '',
+		project_description: '',
+		project_industry: '',
+		project_technologies: [ 'React', 'JavaScript', 'Express', 'Node.js', 'MySQL' ],
+		project_team: {
+			ProjectManager: '',
+			ScrumMaster: '',
+			TechLead: '',
+			WebDeveloper: '',
+			JuniorDeveloper: '',
+			DevOps: ''
+		},
+		project_resources: '',
+		employee_views: '',
+		request_access: ''
+	},
+	{
+		id: 3,
+		poject_name: 'Hoteliq',
+		progect_image: Hoteliq,
+		project_location: 'Paris',
+		project_type: 'Web Application',
+		project_description:
+			'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et',
+		project_industry: 'Hospitality',
+		project_technologies: [ 'JavaScript', 'React', 'MUI', 'Node.js', 'MySQL' ],
+		project_team: {
+			ProjectManager: 'Peter White',
+			ScrumMaster: 'John Smith',
+			TechLead: 'Maria Styles',
+			WebDeveloper: 'Liam Huff',
+			JuniorDeveloper: 'Mario Mendez',
+			DevOps: 'Luis Baker'
+		},
+		project_resources: '',
+		employee_views: 12,
+		request_access: ''
+	},
+	{
+		id: 4,
+		poject_name: '',
+		progect_image: '',
+		project_location: '',
+		project_type: '',
+		project_description: '',
+		project_industry: '',
+		project_technologies: [ 'React', 'JavaScript', 'Express', 'Node.js', 'MySQL' ],
+		project_team: {
+			ProjectManager: '',
+			ScrumMaster: '',
+			TechLead: '',
+			WebDeveloper: '',
+			JuniorDeveloper: '',
+			DevOps: ''
+		},
+		project_resources: '',
+		employee_views: '',
+		request_access: ''
+	}
 ];
