efea7028d7b89951952b6fd6dd5bfce9fd56fe15
Billy <billy.m.nightingale@gmail.com>
2022-06-29 17:23:31 +0200

    added project details

--- src/projectData.js
+++ src/projectData.js
@@ -1,28 +1,28 @@
-import React from "react";
+import AutoScripImg from './AutoScript';
 
 
 export const projectData = [{
     id: 1,
     poject_name: "AutoScript",
-    progect_image: "",
-    project_location: "",
-    project_type: "",
-    project_description: "",
-    project_industry: "",
+    progect_image: AutoScripImg,
+    project_location: "Lyon",
+    project_type: "Web Application",
+    project_description: "AutoScript is a project bringing in a new way to measure tire wear with the aid of web development",
+    project_industry: "Automotive",
     project_technologies: [
         "React", "JavaScript", "Express", "Node.js", "MySQL" 
     ],
     project_team: {
-        ProjectManager: "",
-        ScrumMaster: "",
-        TechLead: "",
-        WebDeveloper: "",
-        JuniorDeveloper: "",
-        DevOps: "",
+        ProjectManager: "Tom Cruise",
+        ScrumMaster: "Jane Doe",
+        TechLead: "Mark zuckerberg",
+        WebDeveloper: "Elon Musk",
+        JuniorDeveloper: "Bill Gates",
+        DevOps: "Steve Carrell",
     },
     project_resources: "",
-    employee_views: "",
-    request_access: ""
+    employee_views: 10,
+    request_access: "Unavailable"
 },
 {
     id: 2,
