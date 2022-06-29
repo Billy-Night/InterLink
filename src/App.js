13ebbedad8e139767da57e17091078a6ea63c28a
Moran <thaler.moran@gmail.com>
2022-06-30 01:09:01 +0200

    Login Registration page is ready with mobile version

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <TopNavBar />
+      {/* <TopNavBar /> */}
       <Routes>
         <Route path="/" element={<LogReg />} />
         <Route path="/log" element={<Login />} />
--- src/pages/LogReg.jsx
+++ src/pages/LogReg.jsx
@@ -1,7 +1,20 @@
+import '../App.css';
+import '../css/pages/LogReg.css';
+
 const LogReg = () => {
   return (
     <div id="LogReg__page">
-      <h1>This is the profile page</h1>
+      <header>
+        <div className="LogReg__hero-img"></div>
+      </header>
+      <main>
+        <h1 className="LogReg__title">Discover your Dream Project Here</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      </main>
+      <footer>
+        <button className="LogReg__btn bright">Register</button>
+        <button className="LogReg__btn dark">Sign In</button>
+      </footer>
     </div>
   );
 };
