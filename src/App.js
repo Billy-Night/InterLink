ba5cf257ec92b698a8be381bdf26ff1ce9b73296
Billy <billy.m.nightingale@gmail.com>
2022-06-29 15:48:47 +0200

    added navigation

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import TopNavBar from './components/TopNavBar.jsx';
 // import MyProvider from './context/MyProvider.js';
 import Login from './pages/Login.jsx';
 import Register from './pages/Register';
-// import Profile from './pages/Home';
+import Profile from './pages/Profile';
+import Home from './pages/Home';
 
 import './App.css';
 
@@ -15,8 +16,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/register" element={<Register />} />
-          {/* <Route path="/profile" element={<Profile />} />
-          <Route path="/home" element={<Home />} /> */}
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/home" element={<Home />} />
         </Routes>
       </div>
   );
