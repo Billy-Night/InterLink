ca28ffbe11f8df424d11d9ae1d65cc0c6ef16e76
Moran <thaler.moran@gmail.com>
2022-06-29 23:31:36 +0200

    added the route for the login register page

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import MyProvider from "./context/MyProvider";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import MyProvider from './context/MyProvider';
 
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import './index.css';
+import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <MyProvider>
     <BrowserRouter>
@@ -15,8 +14,3 @@ root.render(
     </BrowserRouter>
   </MyProvider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ src/pages/LogReg.jsx
@@ -0,0 +1,9 @@
+const LogReg = () => {
+  return (
+    <div id="LogReg__page">
+      <h1>This is the profile page</h1>
+    </div>
+  );
+};
+
+export default LogReg;
