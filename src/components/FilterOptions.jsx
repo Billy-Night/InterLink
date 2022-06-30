babfe6a03ea5f429b7c9fdc5294cf9861e74cd39
David Alexander Pfeiffer <david.pfeiffer971@gmail.com>
2022-06-30 15:49:29 +0200

    best filter ever (not really)

--- src/components/FilterOptions.jsx
+++ src/components/FilterOptions.jsx
@@ -1,41 +1,68 @@
+import { Link } from "react-router-dom";
+import "./FilterOptions.css";
 
-import { Link } from 'react-router-dom';
+const FilterOptions = (props) => {
+  const { filterOptions, setFilterOptions } = props;
 
-const FilterOptions = () => {
-    
+  const availableFilters = {
+    technologies: [
+      "JavaScript",
+      "Sass",
+      "MySQL",
+      "Java",
+      "MongoDB",
+      "React",
+      "Node.js",
+      "Python",
+      "Angular",
+      "C++",
+    ],
+    industries: ["IT"],
+    locations: [],
+  };
 
-    return (
-        <div>
-            <p>This is the filters</p>
-            <ul>
-                <li>
-                    <p>Technologies</p>
-                    <button>JavaScript</button>
-                    <button>Sass</button>
-                    <button>MySQL</button>
-                    <button>Java</button>
-                    <button>MongoDB</button>
-                    <button>React</button>
-                    <button>Node.js</button>
-                    <button>Python</button>
-                    <button>Angular</button>
-                    <button>C++</button>
-                </li>
-                <li>
-                    <p>Industry</p>
-                    <button>IT</button>
-                    <button></button>
-                </li>
-                <li>
-                </li>
-            </ul>
+  return (
+    <div>
+      <p>This is the filters</p>
+      <ul>
+        <p>Technologies</p>
 
-            <button>By Locations</button>
-            <button>Availability</button>
-            <button>Industry</button>
-            <button></button>
-        </div>
-    )
-}
+        {availableFilters.technologies.map((tech) => {
+          return (
+            <li className="list-item">
+              <span
+                onClick={() => {
+                  setFilterOptions({
+                    technologies: filterOptions.technologies.includes(tech)
+                      ? filterOptions.technologies.pop(tech)
+                      : filterOptions.technologies.push(tech),
+                    ...filterOptions,
+                  });
+                }}
+                className={`chip${
+                  filterOptions.technologies.includes(tech) ? " selected" : ""
+                }`}
+              >
+                {tech}
+              </span>
+            </li>
+          );
+        })}
+
+        <li>
+          <p>Industry</p>
+          <button></button>
+          <button></button>
+        </li>
+        <li></li>
+      </ul>
+
+      <button>By Locations</button>
+      <button>Availability</button>
+      <button>Industry</button>
+      <button></button>
+    </div>
+  );
+};
 
 export default FilterOptions;
