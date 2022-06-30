e6352d5bc89b4765edb09fd8b5c4cb4acbd0bb41
Moran <thaler.moran@gmail.com>
2022-06-30 15:53:20 +0200

    start working on the filter feature

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -7,27 +7,28 @@ import TopNavBar from '../components/TopNavBar';
 
 const Dashboard = () => {
   return (
-    <div id="dashboard__page">
+    <>
       <TopNavBar />
+      <div id="dashboard__page">
+        <header>
+          <h1 className="project-name">Discovery Collection</h1>
+          <div className="search__field-and-btn">
+            <input
+              placeholder="contain text"
+              type="text"
+              className="search__field"
+            />
+            <button className="search__btn" />
+          </div>
+        </header>
 
-      <header>
-        <h1 className="project-name">Discovery Collection</h1>
-        <div className="search__field-and-btn">
-          <input
-            placeholder="contain text"
-            type="text"
-            className="search__field"
-          />
-          <button className="search__btn" />
+        <div className="cards-container">
+          {projectData.map((e, index) => {
+            return <ProjectCard key={index} {...e} />;
+          })}
         </div>
-      </header>
-
-      <div className="cards-container">
-        {projectData.map((e, index) => {
-          return <ProjectCard key={index} {...e} />;
-        })}
       </div>
-    </div>
+    </>
   );
 };
 
